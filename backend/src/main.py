640fd0b7c4e63f4b25753738e1e8064e9bf2e9a7
SudiMango <sudipto.islam2005@gmail.com>
2025-11-16 00:53:20 -0800

    updated main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import test_router
-from .routers import auth_controller
-from .routers import post_controller
-from .routers import friend_controller
+from .routers import auth_controller, test_router,  post_controller, friend_controller
+
 
 app = FastAPI()
 
