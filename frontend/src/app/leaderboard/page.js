4cbdc097ddacafe2fba3f12a338bd40e7b9ad691
Jon Liu <jonliu4242@gmail.com>
2025-11-16 10:41:02 -0800

    p

--- frontend/src/app/leaderboard/page.js
+++ frontend/src/app/leaderboard/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useMemo } from "react"
 import BottomNav from "@/components/ui/BottomNav";
 
 import {
