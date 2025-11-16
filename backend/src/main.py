fe743bfb9fe265b081010441d667cc27f68d4791
SudiMango <sudipto.islam2005@gmail.com>
2025-11-16 00:51:28 -0800

    Added friends crud

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import test_router
 from .routers import auth_controller
 from .routers import post_controller
+from .routers import friend_controller
 
 app = FastAPI()
 
@@ -17,3 +18,4 @@ app.add_middleware(
 app.include_router(test_router.router)
 app.include_router(auth_controller.router)
 app.include_router(post_controller.router)
+app.include_router(friend_controller.router)
